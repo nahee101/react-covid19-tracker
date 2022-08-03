6d54fa776ba58644898b5ad3fd6276c7c27a6f31
nahee101 <bagsj26@gmail.com>
2022-08-03 19:58:59 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Cards, Chart, CountryPicker } from './components';
+
+// css 
+import styles from './App.module.css';
+
+// api
+import { fetchData } from "./api";
+
+class App extends React.Component {
+
+    async componentDidMount() {
+        const data = await fetchData();
+
+        console.log(data);
+    }
+
+    render() {
+        return (
+            <div className={styles.container}>
+                <Cards />
+                <Chart />
+                <CountryPicker />
+            </div>
+        )
+    }
 }
 
 export default App;
